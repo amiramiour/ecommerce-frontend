fc1bfdb8107e8d2b41de211985a0de82f27ef8d7
amiramiour <amiouramirtahar@gmail.com>
2024-11-24 18:16:02 +0100

    edit homrpage

--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -1,18 +1,24 @@
-//src/screens/HomePage.jsx
 import React from 'react';
+import { useNavigate } from 'react-router-dom';  // Importez useNavigate
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import '../styles/HomePage.css';
 
-const HomePage = ({ setCurrentPage }) => {
+const HomePage = () => {
+  const navigate = useNavigate();  // Créez l'instance de navigate
+
+  // Fonction pour gérer le clic sur le bouton
+  const handleRegisterClick = () => {
+    navigate('/register');  // Redirige vers /register
+  };
+
   return (
     <div className="home-page">
-      <Header />
       <div className="home-content">
         <h1>Welcome to the E-commerce App</h1>
         <p>Explore our amazing products and collections!</p>
         <div className="home-links">
-          <button className="home-btn" onClick={() => setCurrentPage("signup")}>
+          <button className="home-btn" onClick={handleRegisterClick}>
             Register Now
           </button>
         </div>
