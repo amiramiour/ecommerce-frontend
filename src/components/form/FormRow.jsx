83658f56abc3192e59c552cc8c4d764efe454a67
amiramiour <amiouramirtahar@gmail.com>
2024-11-24 16:27:30 +0100

    suppression des fichiers inutiles

--- src/components/form/FormRow.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FormRow = ({ children }) => {
-  return <div className="row">{children}</div>;
-};
-
-export default FormRow;
--- src/components/layout/FormWrapper.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FormWrapper = ({ children }) => {
-  return <form className="bg-light p-3">{children}</form>;
-};
-
-export default FormWrapper;
