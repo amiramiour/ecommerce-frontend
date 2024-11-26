ad8fbb5e5dabff57fc05eba2e242f8d66aedac2a
amiramiour <amiouramirtahar@gmail.com>
2024-11-26 19:27:02 +0100

    edit homepage component

--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';  // Importez useNavigate
+import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import '../styles/HomePage.css';
+import ProductCard from "../components/ProductCard";
+import "./../styles/HomePage.css";
+
+import HoodieImage from "../assets/images/hoodie.jpeg";
 
 const HomePage = () => {
-  const navigate = useNavigate();  // Créez l'instance de navigate
-
-  // Fonction pour gérer le clic sur le bouton
-  const handleRegisterClick = () => {
-    navigate('/register');  // Redirige vers /register
-  };
+  const products = [
+    {
+      image: HoodieImage,
+      title: "T-Shirt ",
+      price: "20.00",
+      tags: ["Hot", "Sale"],
+    },
+    {
+      image: HoodieImage,
+      title: "Hoodie ",
+      price: "45.00",
+      tags: ["Best Choice"],
+    },
+    {
+      image: HoodieImage,
+      title: "Jeans ",
+      price: "50.00",
+      tags: ["New"],
+    },
+    {
+      image: HoodieImage,
+      title: "Jacket ",
+      price: "100.00",
+      tags: ["Hot", "Sale"],
+    },
+  ];
 
   return (
     <div className="home-page">
       <div className="home-content">
-        <h1>Welcome to the E-commerce App</h1>
-        <p>Explore our amazing products and collections!</p>
-        <div className="home-links">
-          <button className="home-btn" onClick={handleRegisterClick}>
-            Register Now
-          </button>
+        <h1>Explore Our Clothing Collection</h1>
+        <div className="products-grid">
+          {products.map((product, index) => (
+            <ProductCard
+              key={index}
+              image={product.image}
+              title={product.title}
+              price={product.price}
+              tags={product.tags}
+            />
+          ))}
         </div>
       </div>
       <Footer />
